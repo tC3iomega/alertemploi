ab52def7b8507d45d0dace58580211a1e7c6dda5
Quentin Brugeille <brugeillequentin@gmail.com>
2026-06-18 08:37:45 +0200

    fix: nav du bas visible sur dashboard

---
+++ apps/webapp/src/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import { SmallNavbar } from '../components/smallNavbar';
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <section className="min-h-screen w-screen">
+        <div className="mx-auto max-w-5xl">{children}</div>
+        <SmallNavbar />
+      </section>
+    </>
+  );
+}
